e14ac2eee10edf15cc0d19636020431eff69fc92
Alexandru Bordei <alexandru.bordei@hostway.ro>
2016-05-13 14:33:02 +0300

    Entrypoint now looks at existing env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOCUMENT_ROOT=/var/www/localhost/htdocs
-LOG=/var/log/darkhttpd/access.log
+DOCUMENT_ROOT=${DOCUMENT_ROOT:-"/var/www/localhost/htdocs"}
+LOG=${LOG:-"/var/log/darkhttpd/access.log"}
 
 /usr/bin/darkhttpd $DOCUMENT_ROOT --log $LOG --port 80 --no-server-id
